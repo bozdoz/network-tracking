1ee152136489aef973ca74ebb24a958fed441b94
bozdoz <howaboutben@gmail.com>
2020-11-14 04:24:04 +0000

    adds better errors to spreadsheet Append

--- spreadsheet/spreadsheet.go
+++ spreadsheet/spreadsheet.go
@@ -1,9 +1,11 @@
 package spreadsheet
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"golang.org/x/net/context"
 	"google.golang.org/api/option"
@@ -14,8 +16,11 @@ import (
 // go build -ldflags="-X 'bozdoz.com/spreadsheet.spreadsheetID=abc'"
 var spreadsheetID string
 
-// Spreadsheet actually just appends values to a given spreadsheetId and spreadsheetRange
-func Spreadsheet(values []string) {
+// ErrNoConnection is what happens when internet is down
+var ErrNoConnection error = errors.New("No connection")
+
+// Append adds values to a given spreadsheetId and spreadsheetRange
+func Append(values [2]string) (int, error) {
 	ctx := context.Background()
 
 	// credentials.json must be of type "service_account"
@@ -23,15 +28,21 @@ func Spreadsheet(values []string) {
 	credentialsFile := "credentials.json"
 	srv, err := sheets.NewService(ctx, option.WithServiceAccountFile(credentialsFile))
 
+	// return 500, errors.New("Wow")
+
 	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+		err := fmt.Sprintf("Unable to retrieve Sheets client: %v\n", err)
+		fmt.Println(err)
+		fmt.Println(strings.Contains(err, "no such host"))
+
+		return LogAndReturn(err)
 	}
 
 	if spreadsheetID == "" {
 		spreadsheetID = os.Getenv("SPREADSHEET_ID")
 
 		if spreadsheetID == "" {
-			log.Fatalf("Failed to get SPREADSHEET_ID")
+			return LogAndReturn("Failed to get SPREADSHEET_ID")
 		}
 	}
 
@@ -54,8 +65,21 @@ func Spreadsheet(values []string) {
 
 	resp, err := srv.Spreadsheets.Values.Append(spreadsheetID, spreadsheetRange, &vr).ValueInputOption(valueInputOption).InsertDataOption(insertDataOption).Context(ctx).Do()
 	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+		err := fmt.Sprintf("Unable to retrieve data from sheet: %v", err)
+
+		if strings.Contains(err, "no such host") {
+			fmt.Println("no connection")
+			return 0, ErrNoConnection
+		}
+
+		return LogAndReturn(err)
 	}
 
-	fmt.Println(resp.HTTPStatusCode)
+	return resp.HTTPStatusCode, nil
+}
+
+// LogAndReturn logs and returns int and error
+func LogAndReturn(err string) (int, error) {
+	log.Println(err)
+	return 500, errors.New(err)
 }
